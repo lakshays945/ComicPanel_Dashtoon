02f0efdcfbed7f03b0db24e37d95ce116ef86e97
Lakshay Sharma <92531343+lakshays945@users.noreply.github.com>
2024-06-25 14:32:35 +0530

    Update limiter.js

--- limiter.js
+++ limiter.js
@@ -15,7 +15,7 @@ class Limiter{
     refill(){
         const passedTime = Date.now()-this.lastRefill;
         const tokensToAdd = Math.floor(passedTime/this.interval);
-        console.log("passedTime = " + passedTime);
+        console.log(" = " + passedTime);
         if(tokensToAdd == 0) return;
         this.tokens = Math.min(this.limit,this.tokens+tokensToAdd);
         this.lastRefill = Date.now();
